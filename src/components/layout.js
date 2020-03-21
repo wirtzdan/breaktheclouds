4da3ef64282cb1163f2854e8cf748f889088c02a
Daniel Wirtz <danielwirtzx@gmail.com>
2020-03-21 10:40:28 +0100

    :fire: Remove css

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,8 +3,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import { ThemeProvider, CSSReset } from "@chakra-ui/core"
 
-import "./layout.css"
-
 const Layout = ({ children }) => {
   return (
     <ThemeProvider>
