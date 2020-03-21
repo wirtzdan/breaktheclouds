b351048ed2ab88b9277009f3f69f26d393a93f09
Daniel Wirtz <danielwirtzx@gmail.com>
2020-03-21 11:06:43 +0100

    :sparkles: Initial Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,43 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import { Box, Image, Flex, Link, Text } from "@chakra-ui/core"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Box bg="gray.600" width="100vh%" height="100vh">
+      <Image
+        position="absolute"
+        top="0"
+        right="0"
+        left="0"
+        bottom="0"
+        height="100%"
+        width="100%"
+        src="https://mediadc.brightspotcdn.com/dims4/default/070a5d2/2147483647/strip/true/crop/2287x1201+0+61/resize/1200x630!/quality/90/?url=https%3A%2F%2Fmediadc.brightspotcdn.com%2F32%2F4c%2Fe60eb0044d9596e3f9b1a01f1cbd%2Ftsai.jpg"
+        opacity={0.15}
+        objectFit="cover"
+      />
+      <Flex
+        pos="relative"
+        width="100%"
+        height="100%"
+        justify="center"
+        align="center"
+      >
+        <Link
+          color="white"
+          fontSize="6xl"
+          maxW="60rem"
+          textAlign="center"
+          lineHeight="1"
+          href="https://www.washingtonexaminer.com/policy/foreign/taiwan-plans-to-donate-100-000-hospital-masks-to-us-per-week"
+        >
+          Taiwan plans to donate 100,000 hospital masks to US per week
+        </Link>
+      </Flex>
+    </Box>
   </Layout>
 )
 
