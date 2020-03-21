7d594fca946285c06902a7073d54aabaa4741471
Daniel Wirtz <danielwirtzx@gmail.com>
2020-03-21 11:14:11 +0100

    :sparkles: Add Google Fonts and custom theme

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,11 @@ import React from "react"
 
 import PropTypes from "prop-types"
 import { ThemeProvider, CSSReset } from "@chakra-ui/core"
+import theme from "../theme"
 
 const Layout = ({ children }) => {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
       <CSSReset />
       <main>{children}</main>
     </ThemeProvider>
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { theme } from "@chakra-ui/core"
+
+export default {
+  ...theme,
+  fonts: {
+    body: "Bree Serif, serif",
+    heading: "Bree Serif, serif",
+    mono: "Menlo, monospace",
+  },
+}
