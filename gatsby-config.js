8b6f4fa322740062164e713c5954aeb5d2260b4e
Daniel Wirtz <danielwirtzx@gmail.com>
2020-03-21 14:28:27 +0100

    :sparkels: Small changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
         google: {
-          families: ["Bree Serif", "Rajdhani"],
+          families: ["Rajdhani"],
         },
       },
     },
